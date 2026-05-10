61e39e3458f7419434e399618a3045c4cd3eec35
elacanan354-pixel <elacanan354@gmail.com>
2026-05-10 13:32:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,50 @@ async def heh(ctx, count_heh = 5):
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+    
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+
+@bot.command()
+async def Yardım(ctx):
+    await ctx.send("komutlar: merhaba,heh,joined,toplama")
+
+
+
+
+
+
+@bot.command()
+async def mem(ctx):
+    files= os.listdir("images")
+    selected = random.choice(files)
+    with open(f'images/{selected}', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+    
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''duck komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır.'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
 
 
 bot.run("token girin")
